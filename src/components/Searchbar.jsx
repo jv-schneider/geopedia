b67315fc4fcce2f8ab1ff2fb0c792cb74f32a8aa
jvsch <j.v.schneider@web.de>
2023-09-26 14:29:03 +0200

    small input improvement

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -4,7 +4,7 @@ import "./Searchbar.css";
 const Searchbar = ({ handleChange }) => {
   return (
     <div id="searchbar-container">
-      <input type="text" onClick={handleChange} />
+      <input type="text" onKeyDown={handleChange} />
       {/* <button onClick={handleChange}>Search</button> */}
     </div>
   );
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -11,10 +11,10 @@ class App extends React.Component {
       countryURL: "https://restcountries.com/v3.1/name/germany",
       infoData: [
         {
-          population: "5",
-          capital: "Sindelfingen",
-          region: "Wunderland",
-          altSpellings: ["0", "Lummerland", "0"],
+          population: "0",
+          capital: "0",
+          region: "0",
+          altSpellings: ["0", "0", "0"],
         },
       ],
       pictureData: "",
@@ -29,18 +29,34 @@ class App extends React.Component {
   }
 
   stateChange = (event) => {
-    this.setState({
-      countryURL:
-        "https://restcountries.com/v3.1/name/" +
-        event.target.value.toLowerCase(),
-    });
-    fetch(this.state.countryURL)
-      .then((r) => r.json())
-      .then((r) => {
-        try {
-          if (r[0].hasOwnProperty("capital")) {
-            this.setState({ infoData: r });
-          } else {
+    if (event.key == "Enter") {
+      this.setState({
+        countryURL:
+          "https://restcountries.com/v3.1/name/" +
+          event.target.value.toLowerCase(),
+      });
+      fetch(this.state.countryURL)
+        .then((r) => r.json())
+        .then((r) => {
+          try {
+            if (r[0].hasOwnProperty("capital")) {
+              this.setState({ infoData: r });
+            } else {
+              this.setState({
+                infoData: [
+                  {
+                    population: "0",
+                    capital: "0",
+                    region: "0",
+                    subregion: "0",
+                    altSpellings: ["0", "0", "0"],
+                    timezones: "0",
+                    borders: "0",
+                  },
+                ],
+              });
+            }
+          } catch (error) {
             this.setState({
               infoData: [
                 {
@@ -48,29 +64,15 @@ class App extends React.Component {
                   capital: "0",
                   region: "0",
                   subregion: "0",
-                  altSpellings: ["0", "0", "0"],
+                  altSpellings: ["0", "This is not a country!", "0"],
                   timezones: "0",
                   borders: "0",
                 },
               ],
             });
           }
-        } catch (error) {
-          this.setState({
-            infoData: [
-              {
-                population: "0",
-                capital: "0",
-                region: "0",
-                subregion: "0",
-                altSpellings: ["0", "This is not a country!", "0"],
-                timezones: "0",
-                borders: "0",
-              },
-            ],
-          });
-        }
-      });
+        });
+    }
   };
 
   render() {
